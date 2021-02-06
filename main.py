f559889942daf9de90c8df2a9fec1bba40db2676
Lesha Zabrodskyi <71817380+lehruszab@users.noreply.github.com>
2021-02-06 20:02:14 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait
+ 
+from time import sleep
+ 
+app = Client("my_account")
+ 
+@app.on_message(filters.command("type", prefixes=".") & filters.me)
+def type(_, msg):
+    orig_text = msg.text.split(".type ", maxsplit=1)[1]
+    text = orig_text
+    tbp = "" # to be printed
+    typing_symbol = "▒"
+ 
+    while(tbp != orig_text):
+        try:
+            msg.edit(tbp + typing_symbol)
+            sleep(0.05) # 50 ms
+ 
+            tbp = tbp + text[0]
+            text = text[1:]
+ 
+            msg.edit(tbp)
+            sleep(0.05)
+ 
+        except FloodWait as e:
+            sleep(e.x)
+
+app.run()
